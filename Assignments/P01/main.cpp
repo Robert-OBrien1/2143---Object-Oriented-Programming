106e020a3ab3f053a33d757eab87d7e8f2169a0b
Robert O'Brien <70295373+Robert-OBrien1@users.noreply.github.com>
2020-09-14 23:50:42 -0500

    Remade main.cpp to implement Stack.hpp

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -1,320 +1,218 @@
-///////////////////////////////////////////////////////////////////////////////
-//                   
-// Author:           Robert O'Brien
-// Email:            stolen127@live.com
-// Label:            P01
-// Title:            Program 1 - Stack Upgrade
-// Course:           2143
-// Semester:         Fall 2020
-//
-// Description:
-//       Example implementation of an array based stack that holds integers.
-//
-// Usage:
-//       Nothing special right now.
-//
-// Files:            
-//       None
-/////////////////////////////////////////////////////////////////////////////////
-#include <iostream>
-#include <string>
+/*****************************************************************************
+*
+* Authoer:      Robert O'Brien
+* Email:        stolen127@live.com
+* Label:        P01
+* Title:        Stack Upgrade
+* Course:       2143 OOP
+* Semester:     Fall 2020
+*
+* Description: 
+*       This program implements a linear data structure, stack. This program
+*       is able to push and pop values on and off the stack. Prints the start,
+*       max, and ending size of the stack. If the stack reaches it's max
+        capacity, the program will double the size of the stack to accomdate.
+*        
+* Usage:
+*       Peeling bananas 
+* Files:
+*       main.cpp        : main driver
+*       Stack.cpp       : class header and implementation
+*       input.txt       : commands and values to be read
+* 
+*****************************************************************************/
+
+#include <iostream>	
 #include <fstream>
+#include "Stack.hpp";
 using namespace std;
 
-/**
- * Stack
- *
- * Description:
- *      Integer array based stack implementation
- *
- * Public Methods:
- *      - Stack()
- *      - Stack(int)
- *      - void Push()
- *      - int Pop()
- *      - bool empty()
- *      - bool full()
- *      - void Print()
- *
- * Private Methods:
- *      - None
- *
- * Usage:
- *
- *  Stack S;
- *  S.Push(80);
- *  S.Push(90);
- *  S.Print();
- *  int x = S.Pop();
- *
- */
-class Stack {
-private:
-    int* S;       //array pointer
-    int capacity; //max stack size
-    int top;      //current top (index)
-    int size;     //current num items
-public:
-    /**
-     * Stack:
-     *    Constructor.
-     * Params:
-     *    void
-     *
-     * Returns:
-     *     Void
-     */
-    Stack() {
-        capacity = 10;          // set array size
-        S = new int[capacity];  // allocate new memory
-        top = -1;               // initialize top of stack
-        size = 0;               // set stack to empty
-    }
+/////////////////////////////////////////////////////////
+//            Function prototypes                      //
+/////////////////////////////////////////////////////////
+void pushorpop(fstream &in, Stack &S1);
+void openfiles(fstream &in, ofstream &out);
+void closefiles(fstream& in, ofstream &out);
+void studentheader(ofstream &out);
+void StartSize(Stack &S1, ofstream &out);
+void MaxSize(Stack& S1, ofstream& out);
+void EndSize(Stack& S1, ofstream& out);
 
-    /**
-     * Stack:
-     *    Constructor.
-     * Params:
-     *    int : capacity
-     *
-     * Returns:
-     *     Void
-     */
-    Stack(int cap) {
-        capacity = cap;         // set array size      
-        S = new int[capacity];  // allocate new memory
-        top = -1;               // initialize top of stack
-        size = 0;               // set stack to empty
-    }
+/////////////////////////////////////////////////////////
+//                  MAIN                               //
+/////////////////////////////////////////////////////////
+int main()
+{
+    fstream in;             // input variable
+    ofstream out;           // output variable
+    Stack S1;               // create new stack variable
 
-    /**
-     * Push:
-     *    Push item onto stack.
-     * Params:
-     *    int : data
-     *
-     * Returns:
-     *     Void
-     */
-    void Push(int data) {
-        int i;                // variable for loop index
-        if(FullStack()) {     // check if stack is full
-            int* stack2 = new int[capacity * 2];    // double size of the stack
-            for (i = 0; i < capacity; i++) {        
-                stack2[i] = S[i];                   // store orig values into new stack
-            }
+    openfiles(in, out);     // open input and output files
+    studentheader(out);     // print student header
+    pushorpop(in, S1);      // push or pop to the stack
+    StartSize(S1, out);     // print start size of stack
+    MaxSize(S1, out);       // print max size of stack
+    EndSize(S1, out);       // print end size of stack
+    closefiles(in, out);    // close input and output files
 
-            delete[] S;     // point old stack to new stack
-            S = stack2;     // update capacity of stack
-            capacity *= 2;  // delete old stack
-            
-
-        }
-        
-        top++;              // move top of stack up
-        size++;             // increment size
-        S[top] = data;      // add item to array
-    }
-    
-    /**
-     * Pop:
-     *    remove item from stack.
-     * Params:
-     *    void
-     *
-     * Returns:
-     *     int
-     */
-    int Pop() {
-        if (EmptyStack()) {                     // Check if stack is empty
-            cout << "Error: Stack empty!";      // Print error
-            return -1;
-        }
-        
-        int data = S[top];  // pull item from stack
-        top--;              // shrink the stack
-        size--;             // update our size
-
-        if (HalfStack()) {
-            int i;
-            int* stack2 = new int[capacity / 2];    // half size of the stack
-            for (i = 0; i < capacity; i++) {
-                stack2[i] = S[i];                   // store orig values into new stack
-            }
-            delete[] S;     // delete old stack
-            S = stack2;     // point old stack to new stack
-            capacity / 2;   // update capacity of stack
-            
-            
-        }
-        return data;        // send item back
-    }
-    /**
-    * HalfStack:
-    *       is the stack half capacity?
-    * Params:
-    *       none
-    * Returns:
-    *       bool
-    */
-    bool HalfStack() {
-        if (top == capacity / 2)
-            return 1;
-        else
-            return 0;
-    }
-    /**
-     * Empty:
-     *    is the stack empty?
-     * Params:
-     *    void
-     *
-     * Returns:
-     *     bool : true == stack is empty
-     */
-    bool EmptyStack() {
-        //return size == 0;
-        return top == -1;
-    }
-
-    /**
-     * Full:
-     *    is the stack full?
-     * Params:
-     *    void
-     *
-     * Returns:
-     *     bool : true == stack is full
-     */
-    bool FullStack() {
-        return top == capacity - 1;
-    }
-
-    /**
-     * Print:
-     *    Used so we can inspect our stack.
-     * Params:
-     *    void
-     *
-     * Returns:
-     *     void
-     */
-    void Print() {
-        for (int i = top; i >= 0; i--) {
-            cout << S[i] << endl;
-        }
-    }
-
-    /**
-     * Overloaded ostream <<
-     *    Lets us print a stack using cout
-     * Params:
-     *    ostream &os   : instance of ostream passed in by reference
-     *    const Stack   : instance of stack passed in using const so
-     *                    it cannot be changed
-     *
-     * Returns:
-     *     ostream
-     */
-    friend ostream& operator<<(ostream& os, const Stack s) {
-        os << "Overloaded!!" << endl;
-        for (int i = s.top; i >= 0; i--) {
-            os << s.S[i] << endl;
-        }
-        return os;
-    }
-};
-
-/**
- * Person
- *
- * Description:
- *      Example struct that represents a person.
- *
- * Usage:
- *
- *  Person P1;
- *
- *  P1.fname = "firstName";
- *  P1.lname = "lastName";
- *  P1.age = 99
- *  cout<<P1<<endl;
- *
- *  Person P2("billy","bob",11);
- *  cout<<P2<<endl;
- */
-struct Person {
-    string fname;
-    string lname;
-    int age;
-
-    Person();
-    Person(string, string, int);
-
-    friend ostream& operator<<(ostream& os, const Person p) {
-        os << p.fname << "," << p.lname << " : " << p.age << endl;
-        return os;
-    }
-};
-
-/**
- * Constructor:
- *    Constructs the person structure
- * Params:
- *    void
- *
- * Returns:
- *     void
- */
-Person::Person() {
-    fname = "";
-    lname = "";
-    age = 0;
-}
-
-/**
- * Constructor:
- *    Constructs the person structure
- * Params:
- *    string    : first name
- *    string    : last name
- *    int       : persons age
- *
- * Returns:
- *     void
- */
-Person::Person(string f, string l, int a) {
-    fname = f;
-    lname = l;
-    age = a;
-}
-
-int main() {
-    Stack S1;                   // create new stack variable
-    int num;                    // variable to read in num from input file
-
-    ifstream infile;            // variable to read in from file
-    infile.open("input.txt");   // open input file
-
-    S1.Pop();                   // pop to trigger empty stack message
-    cout << endl;
-
-    while (infile >> num) {     // read in integer to end of file
-        S1.Push(num);           // add integer into stack
-    }
-    
-    cout << "Stack before pop\n";
-
-    S1.Print();                 // print current stack
-
-    S1.Pop();                   // remove an integer from top of stack
-    S1.Pop();
-    S1.Pop();
-
-    cout << endl;
-
-    cout << "Stack after pop\n";
-    S1.Print();                 // print stack after pop
-                               
     return 0;
+}
+
+/////////////////////////////////////////////////////////
+//                 Function Definitions                //
+/////////////////////////////////////////////////////////
+/**
+     * Public: pushorpop
+     *
+     * Description:
+     *      Reads file command and values if "push" command.
+     *      Selects with method from Stack.hpp to use and execute.
+     *
+     * Params:
+     *      fstream     : file input variable
+     *      Stack       : Initialised stack
+     * Returns:
+     *      NONE
+     */
+void pushorpop(fstream &in, Stack &S1)
+{
+    string command;             // command read from input
+    int value;                  // value read from input "push" command
+
+    while (!in.eof())           // read to end of file
+    {
+        in >> command;           // read push or pop  
+        if (command == "push")   
+        {
+            in >> value;         // read in value
+            S1.push(value);      // use stack push method, pass value
+        }
+        else
+            S1.pop();            // if not push, use pop method
+    }
+}
+
+/**
+     * Public: openfiles
+     *
+     * Description:
+     *      Opens input and output files.
+     *
+     * Params:
+     *      fstream     : file input varibale
+     *      ofstream    : file output variable
+     *
+     * Returns:
+     *      NONE
+     */
+void openfiles(fstream &in, ofstream &out)
+{
+    in.open("input.txt");
+    out.open("output.txt");
+}
+
+/**
+     * Public: closefiles
+     *
+     * Description:
+     *      Closes input and output files.
+     *
+     * Params:
+     *      fstream     : file input variable
+     *      ofstream    : file output variable
+     *
+     * Returns:
+     *      NONE
+     */
+void closefiles(fstream &in, ofstream &out)
+{
+    in.close();
+    out.close();
+}
+
+/**
+     * Public: studentheader
+     *
+     * Description:
+     *      Prints student head to screen and output file.
+     *
+     * Params:
+     *      ofstream    : file output variable
+     *
+     * Returns:
+     *      NONE
+     */
+void studentheader(ofstream &out)
+{
+    // print header to screen
+    cout << "Name: Robert O'Brien" << '\n';     
+    cout << "Program: P01 - Stack Upgrade" << '\n';
+    cout << "Date: Sep 15 2020" << '\n';
+    cout << "\n\n";
+
+    // print header to output file
+    out << "Name: Robert O'Brien" << '\n';      
+    out << "Program: P01 - Stack Upgrade" << '\n';
+    out << "Date: Sep 15 2020" << '\n';
+    out << "\n\n";
+}
+
+/**
+     * Public: StartSize
+     *
+     * Description:
+     *      Prints starting size of the stack.
+     *
+     * Params:
+     *      Stack       : array based data structure
+     *      ofstream    : file output variable
+     *
+     * Returns:
+     *      NONE
+     */
+void StartSize(Stack& S1, ofstream &out)
+{
+        // starting size of the stack
+    out << "Start Size: " << S1.StartofStack(); 
+    out << '\n';
+}
+
+/**
+     * Public: MaxSize
+     *
+     * Description:
+     *      Prints maximum size of the stack.
+     *
+     * Params:
+     *      Stack       : array based data structure
+     *      ofstream    : file output variable
+     *
+     * Returns:
+     *      NONE
+     */
+void MaxSize(Stack& S1, ofstream& out)
+{
+        // max size of the stack
+    out << "Max Size: " << S1.MaxofStack();      
+    out << '\n';
+}
+
+/**
+     * Public: EndSize
+     *
+     * Description:
+     *      Prints ending size of the stack.
+     *
+     * Params:
+     *      Stack       : array based data structure
+     *      ofstream    : file output variable
+     *
+     * Returns:
+     *      NONE
+     */
+void EndSize(Stack& S1, ofstream& out)
+{
+        // end size of the stack
+    out << "Ending Size: " << S1.EndofStack();      
+    out << '\n';
 }
